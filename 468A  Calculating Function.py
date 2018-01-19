06ab7f3e43b511a38b8e82e9f6542f9b6302e3a5
Anurag Goel <thechampanurag@gmail.com>
2018-01-19 10:04:36 +0530

    468,479

---
+++ 468A  Calculating Function.py
@@ -0,0 +1,5 @@
+a=input()
+l = a if a%2!=0 else a-1
+x=(a*(a+1))/2
+y=(((l+1)/2)*(1+l))/2
+print x-(2*y)
---
+++ 479A - Expression.py
@@ -0,0 +1,4 @@
+a=int(raw_input())
+b=int(raw_input())
+c=int(raw_input())
+print max([(a+b*c),(a*(b+c)),(a*b*c),((a+b)*c),(a+b+c)])
